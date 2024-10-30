fb17531d3642305bbab3fa59e63ddea85e93d8dc
makooda <andrew@webtecgh.com>
2024-10-30 17:24:38 +0000

    listing all users in the data grid

--- frontend/universe/src/system-parts/universe/MainDashboard.tsx
+++ frontend/universe/src/system-parts/universe/MainDashboard.tsx
@@ -3,18 +3,17 @@ import { Box, CssBaseline } from '@mui/material';
 import AppTheme from '../../shared-theme/AppTheme';
 import AppNavbar from './components/AppNavbar';
 import SideMenu from './components/SideMenu';
-import MainContentArea from './components/MainContentArea';
 import Footer from './components/Footer';
 
-import Home from './components/HomePage'; 
-import UserManagement from './components/UserManagementPage'; 
-import Profile from './components/OrganizationProfilePage'; 
-import Branding from './components/OrganizationBrandingPage'; 
-import Hierarchies from './components/OrganizationHierarchiesPage'; 
-import Setups from './components/OrganizationSetupsPage'; 
+import Home from './HomePage'; 
+import UserManagement from './UserManagementPage'; 
+import Profile from './OrganizationProfilePage'; 
+import Branding from './OrganizationBrandingPage'; 
+import Hierarchies from './OrganizationHierarchiesPage'; 
+import Setups from './OrganizationSetupsPage'; 
 
 
-const Dashboard: React.FC = () => {
+const MainDashboard: React.FC = () => {
   const [selectedComponent, setSelectedComponent] = useState<JSX.Element | null>(null);
   const [searchTerm, setSearchTerm] = useState('');
   const [selectedPath, setSelectedPath] = useState<string>('');
@@ -54,7 +53,7 @@ const Dashboard: React.FC = () => {
         <AppNavbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} onSearch={handleSearch}/>
         <Box display="flex" flexGrow={1}>
           <SideMenu onMenuItemClick={handleMenuItemClick} selectedPath={selectedPath} />
-          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Box component="main" sx={{ flexGrow: 1 }}>
              {selectedComponent}
            </Box>
         </Box>
@@ -64,4 +63,4 @@ const Dashboard: React.FC = () => {
   );
 };
 
-export default Dashboard;
+export default MainDashboard;
